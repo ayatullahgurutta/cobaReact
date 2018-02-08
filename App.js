c56868645b87519a4e144567d9101f8f78eb6a6c
ayatullahgurutta <33905442+ayatullahgurutta@users.noreply.github.com>
2018-02-08 16:03:10 +0700

    Add get & post user

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { Platform, StyleSheet, Text, TextInput, View, Image, Button } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -16,19 +16,85 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    let userUrl = 'https://api.mesosfer.com/api/v2/users';
+    return fetch(userUrl, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "X-Mesosfer-AppId": "tzVOewKnss",
+        "X-Mesosfer-AppKey": "ACNRwDdYOzGekYkDPbd1ffNpEPH0nr8J"
+      }
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  regUser = () => {
+    let userUrl = 'https://api.mesosfer.com/api/v2/users';
+    let payload = {
+      firstname: this.userFirst,
+      lastname: this.userLast,
+      email: this.userMail,
+      password: this.userPass,
+    }
+    console.log(JSON.stringify(payload));
+    return fetch(userUrl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "X-Mesosfer-AppId": "tzVOewKnss",
+        "X-Mesosfer-AppKey": "ACNRwDdYOzGekYkDPbd1ffNpEPH0nr8J"
+      },
+      body: JSON.stringify(payload)
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome!
+          Selamat Datang!
         </Text>
         <Image source={require('./img/V_icon.png')} style={styles.images}/>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Text style={styles.titles}>
+          User Reg
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text>First Name: </Text>
+        <TextInput
+          onChangeText={(text) => this.userFirst = text}
+          style={{width: 150, height: 30}}/>
+        <Text>Last Name: </Text>
+        <TextInput
+          onChangeText={(text) => this.userLast = text}
+          style={{width: 150, height: 30}}/>
+        <Text>Email: </Text>
+        <TextInput
+          onChangeText={(text) => this.userMail = text}
+          style={{width: 150, height: 30}}/>
+        <Text>Password: </Text>
+        <TextInput
+          secureTextEntry={true}
+          onChangeText={(text) => this.userPass = text}
+          style={{width: 150, height: 30}}/>
+        <Button
+          onPress={this.regUser}
+          title="Register"
+          accessibilityLabel="Registering User"
+          style={styles.images}
+        />
       </View>
     );
   }
@@ -39,21 +105,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#B0E0E6',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 24,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  titles: {
+    fontSize: 20,
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
   images: {
-    width: 150,
-    height: 150,
-    marginBottom: 20
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  btnReg: {
+    color: '#50C878',
+    marginTop: 10,
   }
 });
